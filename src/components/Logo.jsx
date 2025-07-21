7aac4b79135546e5d127e62389c51b8de87dc00d
LeonEidelman <leoneidelman09@gmail.com>
2025-07-21 14:11:27 +0300

    index.css update

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,9 +1,9 @@
-export default function Logo({}) {
+export default function Logo() {
   return (
     <>
       <img
         style={{
-          height: '1.5rem',
+          height: '1.7rem',
           marginRight: '0.3rem',
         }}
         src="../public/note.svg"
