995280feb38431737f2a7e082c3bf1cf555c52ba
LeonEidelman <leoneidelman09@gmail.com>
2025-07-22 12:08:16 +0300

    initialize the noteEditor jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,16 @@ import { useState } from 'react';
 
 import './App.css';
 import SideMenu from './components/SideMenu/SideMenu';
+import NoteEditor from './components/NoteEditor/NoteEditor';
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
+    <div className="App-wrapper">
       <SideMenu />
-    </>
+      <NoteEditor />
+    </div>
   );
 }
 
---
+++ src/components/NoteEditor/NoteEditor.jsx
@@ -0,0 +1,26 @@
+import './NoteEditor.css';
+
+const NoteEditor = () => {
+  return (
+    <div className="Editor-wrapper">
+      <div className="save-indicator">Saved!</div>
+      <div>
+        Title
+        <input
+          type="text"
+          placeholder="Your title here"
+        />
+      </div>
+      <div>
+        Note
+        <textarea
+          placeholder="You content here"
+          name=""
+          id=""
+        ></textarea>
+      </div>
+    </div>
+  );
+};
+
+export default NoteEditor;
