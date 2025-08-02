15e1c9d80a4dc3c394f74d41a193a9ab5467c187
LeonEidelman <leoneidelman09@gmail.com>
2025-08-02 19:16:01 +0300

    minor svg fix

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,15 +1,10 @@
 import SavedIndicator from './SavedIndicator';
+import '../../public/note.svg';
 
 export default function Logo({ onBack, showSaved }) {
   return (
     <div className="flex m-3">
       {onBack && (
-        
-<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-  <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 15.75 3 12m0 0 3.75-3.75M3 12h18" />
-</svg>
-
-
         <button
           className="btn size-4 text-primary-content text-3xl"
           onClick={onBack}
@@ -18,7 +13,7 @@ export default function Logo({ onBack, showSaved }) {
         </button>
       )}
       <img
-        src="./note.svg"
+        src="/note.svg"
         alt="logo"
         className="size-6 m-2"
       />
