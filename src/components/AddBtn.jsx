da8941c423cbd37b0f2731e5123dd2cc7d48eb64
LeonEidelman <leoneidelman09@gmail.com>
2025-08-04 15:56:05 +0300

    AddBtn text color change

--- src/components/AddBtn.jsx
+++ src/components/AddBtn.jsx
@@ -1,16 +1,16 @@
+import { Plus } from 'phosphor-react';
 function AddBtn({ onAddNote }) {
   return (
     <>
       <button
-        className="btn bg-gradient-to-bl from-accent to-secondary text-yellow-50 
+        className="btn bg-gradient-to-bl from-accent to-secondary text-accent-content 
         hover:bg-gradient-to-br focus:ring-2 focus:outline-none focus:ring-base-300` dark:focus:ring-base-300 font-medium
         capitalize w-32 ml-auto mr-6 my-6"
         onClick={onAddNote}
       >
-        <img
-          className="plus-svg"
-          src="/src/assets/plus.svg"
-          alt=""
+        <Plus
+          size={20}
+          weight="bold"
         />
         add Note
       </button>
