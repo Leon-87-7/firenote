8f5aa136d83a78b7da5bbfadb5357c04672ab8e4
LeonEidelman <leoneidelman09@gmail.com>
2025-08-05 12:25:22 +0300

    boxShadow is functional and use on the ThemeContext

--- src/components/NoteEditor.jsx
+++ src/components/NoteEditor.jsx
@@ -1,8 +1,10 @@
 import { useState, useEffect } from 'react';
 import toast from 'react-hot-toast';
 import ThemeToggle from './ThemeToggle';
+import { useTheme } from '../context/ThemeContext';
 
 function NoteEditor({ note, onUpdateNote, showSaved, isMobile }) {
+  const { theme } = useTheme();
   const [title, setTitle] = useState('');
   const [content, setContent] = useState('');
 
@@ -18,17 +20,30 @@ function NoteEditor({ note, onUpdateNote, showSaved, isMobile }) {
 
   const getThemeColor = (colorProp) => {
     const computedStyle = getComputedStyle(document.documentElement);
-    return computedStyle.getPropertyValue(colorProp).trim();
+    const value = computedStyle.getPropertyValue(colorProp).trim();
+    return value;
   };
+
+  // const secondary = getThemeColor('--s');
+  const accent = getThemeColor('--a');
+  const neutral = getThemeColor('--n');
+  const success = getThemeColor('--su');
+  const warningContent = getThemeColor('--wac');
+
+  const getShadowColor = (currentTheme) => {
+    return currentTheme === 'cordovanChalkTheme'
+      ? `rgba(41, 37, 36, 0.3)`
+      : `rgba(240, 227, 224, 0.2)`;
+  };
+
+  const shadowBoxStyle = {
+    boxShadow: `0px 0px 11px -5px ${getShadowColor(theme)}`,
+  };
+  console.log('Current theme:', theme);
+  console.log('Shadow style:', shadowBoxStyle);
 
   useEffect(() => {
     if (showSaved) {
-      const secondary = getThemeColor('--s');
-      const accent = getThemeColor('--a');
-      const neutral = getThemeColor('--n');
-      const success = getThemeColor('--su');
-      const warningContent = getThemeColor('--wac');
-
       toast.success('saved!', {
         position: 'top-right',
         style: {
@@ -46,7 +61,7 @@ function NoteEditor({ note, onUpdateNote, showSaved, isMobile }) {
         },
       });
     }
-  }, [showSaved]);
+  }, [showSaved, accent, neutral, success, warningContent]);
 
   const handleTitleBlur = () => {
     if (note && title !== note.title) {
@@ -60,9 +75,55 @@ function NoteEditor({ note, onUpdateNote, showSaved, isMobile }) {
     }
   };
 
+  if (isMobile) {
+    // Mobile screen
+    return (
+      <>
+        <div className="h-full flex flex-col pl-4 pr-6 max-md:shadow-none">
+          <label className="form-control">
+            <div className="label">
+              <span className="label-text font-medium text-lg">
+                Title:
+              </span>
+            </div>
+            <input
+              type="text"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+              onBlur={handleTitleBlur}
+              placeholder="Your title here"
+              className="input input-bordered w-full input-secondary "
+            />
+          </label>
+
+          <label className="form-control flex-1 flex flex-col mt-2">
+            <div className="label">
+              <span className="label-text font-medium text-lg">
+                Note:
+              </span>
+            </div>
+            <textarea
+              className="textarea textarea-bordered w-full flex-1 resize-none rounded-lg textarea-accent"
+              style={{ minHeight: '28rem' }}
+              value={content}
+              onChange={(e) => setContent(e.target.value)}
+              onBlur={handleContentBlur}
+              placeholder="Your content here"
+            ></textarea>
+          </label>
+        </div>
+      </>
+    );
+  }
+
   return (
+    // Desktop screen
     <>
-      <div className="h-full flex flex-col mx-8 my-4 p-8 rounded-3xl max-md:shadow-none shadow-md shadow-primary-content">
+      <div
+        className="h-full flex flex-col mx-8 my-4 p-8 rounded-3xl"
+        style={shadowBoxStyle}
+        // shadow-md shadow-primary-content"
+      >
         {!isMobile && <ThemeToggle />}
         <label className="form-control">
           <div className="label">
@@ -76,7 +137,7 @@ function NoteEditor({ note, onUpdateNote, showSaved, isMobile }) {
             onChange={(e) => setTitle(e.target.value)}
             onBlur={handleTitleBlur}
             placeholder="Your title here"
-            className="input input-bordered w-full max-md:input-secondary "
+            className="input input-bordered w-full"
           />
         </label>
 
@@ -87,8 +148,7 @@ function NoteEditor({ note, onUpdateNote, showSaved, isMobile }) {
             </span>
           </div>
           <textarea
-            className="textarea textarea-bordered w-full flex-1 resize-none rounded-lg max-md:textarea-accent"
-            style={{ minHeight: '34rem' }}
+            className="textarea textarea-bordered w-full flex-1 resize-none rounded-lg "
             value={content}
             onChange={(e) => setContent(e.target.value)}
             onBlur={handleContentBlur}
