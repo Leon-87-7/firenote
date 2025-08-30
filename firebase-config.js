1227c89c3b9ba4a0473c7aa66be9d7f98e995718
LeonEidelman <leoneidelman09@gmail.com>
2025-08-30 17:30:09 +0300

    fireBaseConfig renaming  Vite's env vars;

--- firebase-config.js
+++ firebase-config.js
@@ -1,19 +1,22 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.VITE_FIREBASE_API_KEY,
-  authDomain: 'notes-app-ab225.firebaseapp.com',
-  projectId: 'notes-app-ab225',
-  storageBucket: 'notes-app-ab225.firebasestorage.app',
-  messagingSenderId: '318661635901',
-  appId: '1:318661635901:web:1773193936f99859a49f54',
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env
+    .VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 // Initialize Cloud Firestore and get a reference to the service
 export const db = getFirestore(app); // Pass the initialized app to getFirestore()
+export const auth = getAuth(app);
